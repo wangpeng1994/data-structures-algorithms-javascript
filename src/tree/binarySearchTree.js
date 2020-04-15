898a83b90ddbd2c1d2cde1896e732b08644cbad9
wangpeng1994 <947034046@qq.com>
2020-04-15 13:19:39 +0800

    BST的前中后序遍历

--- src/tree/binarySearchTree.js
+++ src/tree/binarySearchTree.js
@@ -9,7 +9,7 @@ function BinarySearchTree() {
   var root = null;
 
   /**
-   * 辅助节点的插入
+   * 节点插入辅助函数
    *
    * @param {Node} node 当前节点
    * @param {Node} newNode 待插入的节点
@@ -30,6 +30,11 @@ function BinarySearchTree() {
     }
   };
 
+  /**
+   * 插入新的键
+   *
+   * @param key
+   */
   this.insert = function (key) {
     var newNode = new Node(key);
 
@@ -40,13 +45,72 @@ function BinarySearchTree() {
     }
   };
 
+  /**
+   * 中序遍历辅助函数
+   *
+   * @param node
+   * @param callback
+   */
   var inOrderTraverseNode = function (node, callback) {
     if (node !== null) {
-
+      inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      inOrderTraverseNode(node.right, callback);
     }
   };
 
+  /**
+   * 中序遍历
+   *
+   * @param callback
+   */
   this.inOrderTraverse = function (callback) {
+    inOrderTraverseNode(root, callback);
+  };
 
+  /**
+   * 先序遍历辅助函数
+   *
+   * @param node
+   * @param callback
+   */
+  var preOrderTraverseNode = function (node, callback) {
+    if (node !== null) {
+      callback(node.key);
+      preOrderTraverseNode(node.left, callback);
+      preOrderTraverseNode(node.right, callback);
+    }
+  };
+
+  /**
+   * 先序遍历
+   *
+   * @param callback
+   */
+  this.preOrderTraverse = function (callback) {
+    preOrderTraverseNode(root, callback);
+  };
+
+  /**
+   * 后序遍历辅助函数
+   *
+   * @param node
+   * @param callback
+   */
+  var postOrderTraverseNode = function (node, callback) {
+    if (node !== null) {
+      postOrderTraverseNode(node.left, callback);
+      postOrderTraverseNode(node.right, callback);
+      callback(node.key);
+    }
+  };
+
+  /**
+   * 后序遍历
+   *
+   * @param callback
+   */
+  this.postOrderTraverse = function (callback) {
+    postOrderTraverseNode(root, callback);
   };
 }
